f7ea25bd3a140714cbc63d4f40a24239526b2c0b
LauraArroyo10 <154952947+LauraArroyo10@users.noreply.github.com>
2025-05-28 13:05:56 -0600

    First commit(uploading our project)

---
+++ src/main/java/com/SystemHestia/SystemhestiaApplication.java
@@ -0,0 +1,13 @@
+package com.SystemHestia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemhestiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemhestiaApplication.class, args);
+	}
+
+}
