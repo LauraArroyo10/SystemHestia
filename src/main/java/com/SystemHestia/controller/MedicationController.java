948d6f886ea2545fb94be25698f4c19141c3fda0
DayaOrozco <DAYANA.OROZCORODRIGUEZ@ucr.ac.cr>
2025-06-08 17:14:05 -0600

    MedicationController commit Dayana

--- src/main/java/com/SystemHestia/controller/MedicationController.java
+++ src/main/java/com/SystemHestia/controller/MedicationController.java
@@ -1,6 +1,48 @@
 package com.SystemHestia.controller;
+import com.SystemHestia.model.Medication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 public class MedicationController {
+    @Autowired
+    MedicationService service;
 
+    //GET ALL
+    @GetMapping
+    public ArrayList<Medication> getAll(){
+        return service.getAll();
+    }
+    //GET ALL BY ID
+    @GetMapping ("{id}")
+    public Medication get(@PathVariable int id){
+        return service.findById(id);
+    }
+
+
+    //POST
+    @PostMapping
+    public Medication post(@RequestBody Medication medication){
+        return service.add(medication);}
+
+    //PUT
+    @PutMapping
+    public Medication put(@RequestBody Medication medication ){
+        return service.update(medication);
+    }
+
+    //DELETE
+    @DeleteMapping("{id}")
+    public Medication delete(@PathVariable int id){
+        return service.delete(id);
+    }
+
+    //PATCH
+    @PatchMapping
+    public Medication patchSpace (@RequestBody Medication medication) {
+
+        return service.edit(medication);
+    }
 
 }
